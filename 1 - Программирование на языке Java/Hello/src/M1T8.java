64cf98aa5e6a1d231363ddff591d5044b5849d21
tankalxat34 <tankalxat34@gmail.com>
2025-09-07 21:52:52 +0300

    Update M1T8.java

--- 1 - Программирование на языке Java/Hello/src/M1T8.java
+++ 1 - Программирование на языке Java/Hello/src/M1T8.java
@@ -1,3 +1,10 @@
 public class M1T8 {
-
+    public static void main(String[] args) {
+        int[] arr = {15, 27, 3, 344, 17 };
+        int summ = 0;
+        for (int i: arr) {
+            summ += i;
+        }
+        System.out.println(summ / arr.length);
+    }
 }
