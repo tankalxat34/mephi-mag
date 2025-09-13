811083d823b97dca8f485a306ccdaf5e353641ee
tankalxat34 <tankalxat34@gmail.com>
2025-09-13 12:08:56 +0300

    .

---
+++ 1 - Программирование на языке Java/Hello/src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    public void build() {
+        System.out.println("You must build a new car");
+    }
+
+    public void buy() {
+        System.out.println("I will buy this car");
+    }
+}
